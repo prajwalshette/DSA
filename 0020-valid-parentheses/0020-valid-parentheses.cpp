4bcc0c245cce11a22ea860d2871cf7dcf9b56eb6
Prajwal Shette <90400896+prajwalshette@users.noreply.github.com>
2023-04-10 07:56:46 +0530

    Time: 0 ms (100.00%), Space: 6.2 MB (81.54%) - LeetHub

---
+++ 0020-valid-parentheses/0020-valid-parentheses.cpp
@@ -0,0 +1,44 @@
+class Solution {
+public:
+    bool isValid(string ss) {
+        
+        stack<char> s;
+     for(int i=0; i<ss.length(); i++) {
+         
+         char ch = ss[i];
+         
+         //if opening bracket, stack push
+         //if close bracket, stacktop check and pop
+         
+         if(ch == '(' || ch == '{' || ch == '['){
+             s.push(ch);
+         }
+         else
+         {
+             //for closing bracket
+             if(!s.empty()) {
+                  char top = s.top();
+                  if( (ch == ')' && top == '(') || 
+                     ( ch == '}' && top == '{') || 
+                     (ch == ']' && top == '[') ) {
+                      s.pop();
+                  }
+                 else
+                 {
+                     return false;
+                 }
+             }
+             else
+             {
+                 return false;
+             } 
+         }  
+     }
+    
+    if(s.empty())
+        return true;
+    else
+        return false;
+        
+    }
+};
