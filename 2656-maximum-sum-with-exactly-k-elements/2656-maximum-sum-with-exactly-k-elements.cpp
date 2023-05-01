bb778d324d78c4f35c04100ec8fab3abd8488689
Prajwal Shette <90400896+prajwalshette@users.noreply.github.com>
2023-05-01 08:45:09 +0530

    Time: 40 ms (100.00%), Space: 70.6 MB (100.00%) - LeetHub

--- 2656-maximum-sum-with-exactly-k-elements/2656-maximum-sum-with-exactly-k-elements.cpp
+++ 2656-maximum-sum-with-exactly-k-elements/2656-maximum-sum-with-exactly-k-elements.cpp
@@ -5,10 +5,11 @@ public:
       int n = nums.size();
       sort(nums.begin(),nums.end());
       int sum = 0;
-      while(k--)
+      while(k)
       {
             sum += nums[n-1];
             nums[n-1] = nums[n-1]+1;
+            k--;
         }
        
       return sum;
