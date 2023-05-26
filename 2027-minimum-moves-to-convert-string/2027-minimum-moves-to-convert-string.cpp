d0bba559cd6ed23a228159e0061eadd6660e686a
Prajwal Shette <90400896+prajwalshette@users.noreply.github.com>
2023-05-26 08:10:32 +0530

    Time: 0 ms (100.00%), Space: 6.4 MB (39.80%) - LeetHub

---
+++ 2027-minimum-moves-to-convert-string/2027-minimum-moves-to-convert-string.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int minimumMoves(string s) {
+        
+        int ans=0;
+        for(int i=0;i<s.size();i++)
+        {
+            if(s[i]=='X')
+            {
+                ans++;
+                i=i+2;
+            }
+        }
+        return ans;
+    }
+};
