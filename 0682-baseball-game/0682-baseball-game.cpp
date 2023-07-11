69d943f759bd52c0ece916e4f6614684189ef23a
Prajwal Shette <90400896+prajwalshette@users.noreply.github.com>
2023-07-11 19:56:19 +0530

    Time: 4 ms (81.47%), Space: 8.3 MB (86.02%) - LeetHub

---
+++ 0682-baseball-game/0682-baseball-game.cpp
@@ -0,0 +1,38 @@
+class Solution {
+public:
+    int calPoints(vector<string>& ops) {
+        int n=ops.size();
+        stack<int>st;
+        for(int i=0;i<n;i++)
+        {
+           if(ops[i] == "C") 
+           {
+               st.pop();
+           }
+           else if(ops[i]=="D")
+           {
+               st.push(st.top()*2);
+           }
+           else if(ops[i]=="+")
+           {
+               int f=st.top();
+               st.pop();
+               int s=st.top();
+               st.push(f);
+               st.push(f+s);
+           }
+           else
+           {
+              st.push(stoi(ops[i]));
+           }
+
+        }
+        int sum=0;
+        while(!st.empty())
+        {
+            sum+=st.top();
+            st.pop();
+        }
+        return sum;
+    }
+};
